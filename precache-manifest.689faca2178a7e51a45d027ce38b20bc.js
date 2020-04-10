9cc30c75ef90653b1c2f27e969dab96fb4fe28e6
James Villanueva <villan31@gmail.com>
2020-04-10 11:06:01 -0700

    Updates

---
+++ precache-manifest.689faca2178a7e51a45d027ce38b20bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ba5c1647ca39cc7a1913178f44d8fd1",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "5b14aafed5e87720dd1b",
+    "url": "/monsters-rolodex/static/css/main.ced94439.chunk.css"
+  },
+  {
+    "revision": "30436d15d072549122d4",
+    "url": "/monsters-rolodex/static/js/2.0d232ef2.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters-rolodex/static/js/2.0d232ef2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b14aafed5e87720dd1b",
+    "url": "/monsters-rolodex/static/js/main.dc68f601.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
